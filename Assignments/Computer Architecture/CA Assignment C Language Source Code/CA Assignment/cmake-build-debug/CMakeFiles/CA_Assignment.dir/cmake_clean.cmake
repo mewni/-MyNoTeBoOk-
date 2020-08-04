e91589837e754a55de5ff16548125f28592271b3
Mewni Lasanya Alahakoon <56906579+Mewni@users.noreply.github.com>
2020-08-04 14:43:14 +0530

    Added files

---
+++ Assignments/Computer Architecture/CA Assignment C Language Source Code/CA Assignment/cmake-build-debug/CMakeFiles/CA_Assignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CA_Assignment.exe"
+  "CA_Assignment.exe.manifest"
+  "CA_Assignment.pdb"
+  "CMakeFiles/CA_Assignment.dir/main.c.obj"
+  "libCA_Assignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CA_Assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
